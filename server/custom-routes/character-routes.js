f2345884c969b9c0769d4f0ac51176925bb1ca95
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2017-09-22 14:16:19 -0600

    newNPC Route now generates NPC

--- server/custom-routes/character-routes.js
+++ server/custom-routes/character-routes.js
@@ -29,30 +29,59 @@ module.exports = {
       if (req.body.race) {
         Races.findOne({ name: req.body.race }).then(r => {
           race = r
+          if( r == null ){
+            res.send(handleResponse(action, null, "ERROR: Race: '"+ req.body.race +"' Not found"))
+            return
+          }
+          if (req.body.background) {
+            Backgrounds.findOne({ name: req.body.background }).then(b => {
+              background = b
+              if( b == null ){
+                res.send(handleResponse(action, null,  "ERROR: Race: '"+ req.body.race +"' Not found"))
+                return
+              }
+              mapNPC(npc, race, background)
+              res.send(handleResponse(action, npc))
+            })
+          }
+          else {
+            Backgrounds.find({}).then(b => {
+              let bIndex = Math.floor(Math.random() * (b.length - 1))
+              background = b[bIndex]
+              mapNPC(npc, race, background)
+              res.send(handleResponse(action, npc))
+            })
+          }
+        }).then(()=>{
+          mapNPC(npc, race, background)
+          res.send(handleResponse(action, npc))
         })
       }
       else {
         Races.find({}).then(r => {
           let rIndex = Math.floor(Math.random() * (r.length - 1))
           race = r[rIndex]
-
+          if (req.body.background) {
+            Backgrounds.findOne({ name: req.body.background }).then(b => {
+              background = b
+              if( b == null ){
+                res.send(handleResponse(action, null, "ERROR: Background: '"+ req.body.background +"' Not found"))
+                return
+              }
+              mapNPC(npc, race, background)
+              res.send(handleResponse(action, npc))
+            })
+          }
+          else {
+            Backgrounds.find({}).then(b => {
+              let bIndex = Math.floor(Math.random() * (b.length - 1))
+              background = b[bIndex]
+              mapNPC(npc, race, background)
+              res.send(handleResponse(action, npc))
+            })
+          }
         })
-      }
-      //assigns background
-      if (req.body.background) {
-        Races.findOne({ name: req.body.background }).then(b => {
-          background = b
-        })
-      }
-      else {
-        Backgrounds.find({}).then(b => {
-          let bIndex = Math.floor(Math.random() * (b.length - 1))
-          background = b[bIndex]
-        })
-      }
-      mapNPC(npc, race, background)
-
-      res.send(handleResponse(action, npc))
+      }   
     }
   }
 }
@@ -74,15 +103,19 @@ function handleResponse(action, data, error) {
 function mapNPC(npc, race, background) {
   //maps race
   npc.race = race.name
-  npc.raceDescription = race.description
+  npc.raceDescription = race.desc
   npc.ability = race.ability
   npc.age = Math.floor(Math.random() * (race.age[1] - race.age[0]) + race.age[0])
   npc.speed = race.speed
   npc.languages = race.languages
   npc.racials = race.racials
-  let subRaces = Object.keys(race.sub)
-  let subIndex = Math.floor(Math.random() * (subRaces.length - 1))
-  npc.sub = race.sub[subIndex]
+  if(Object.keys(race.sub).length>0){
+    let subRaces = Object.keys(race.sub)
+    let subIndex = Math.floor(Math.random() * (subRaces.length - 1))
+  
+    npc.sub = race.sub[subRaces[subIndex]]
+    npc.sub.subRace = subRaces[subIndex]
+  }
 
   //maps background
   npc.background = background.name
@@ -95,7 +128,7 @@ function mapNPC(npc, race, background) {
   let traitIndex = Math.floor(Math.random() * (background.traits.length - 1))
   npc.trait = background.traits[traitIndex]
   let idealIndex = Math.floor(Math.random() * (background.ideals.length - 1))
-  npc.ideal = background.ideals[idealsIndex]
+  npc.ideal = background.ideals[idealIndex]
   let bondIndex = Math.floor(Math.random() * (background.bonds.length - 1))
   npc.bond = background.bonds[bondIndex]
   let flawIndex = Math.floor(Math.random() * (background.flaws.length - 1))
